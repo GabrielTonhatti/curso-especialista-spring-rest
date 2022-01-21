2da5fe1bdf3afc523912b14ec11ae5b9c055505f
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-01-20 22:58:07 -0300

    Initializing chapter of JPA and Hibernete

---
+++ 3-introducao-jpa-hibernete/algafood-api/src/main/java/com/algaworks/algafood/di/notificao/NivelUrgencia.java
@@ -0,0 +1,8 @@
+package com.algaworks.algafood.di.notificao;
+
+public enum NivelUrgencia {
+
+    URGENTE,
+    SEM_URGENCIA
+
+}
---
+++ 3-introducao-jpa-hibernete/algafood-api/src/main/java/com/algaworks/algafood/di/notificao/NotificadorEmail.java
@@ -0,0 +1,23 @@
+package com.algaworks.algafood.di.notificao;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Profile("prod")
+@TipoDoNotificador(value = NivelUrgencia.SEM_URGENCIA)
+public class NotificadorEmail implements Notificador {
+
+    @Autowired
+    private NotificadorProperties properties;
+
+    @Override
+    public void notificar(Cliente cliente, String mensagem) {
+        System.out.println("Host: " + properties.getHostServidor());
+        System.out.println("Porta: " + properties.getPortaServidor());
+
+        System.out.printf("Notificando %s através do e-mail %s: %s\n",
+                cliente.getNome(), cliente.getEmail(), mensagem);
+    }
+}
