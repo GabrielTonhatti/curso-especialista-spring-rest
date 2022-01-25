809fffcc2039369aea7a4062deb5a06462dca6d2
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-01-24 22:20:51 -0300

    Negociando o media type do payload do POST com Content-Type

--- 4-Rest-com-Spring/algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ 4-Rest-com-Spring/algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,14 +4,10 @@ import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,13 +32,17 @@ public class CozinhaController {
     public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
         Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
 
-        if(cozinha != null) {
-            return ResponseEntity.ok(cozinha);
-        }
+        if (cozinha != null) return ResponseEntity.ok(cozinha);
 
 //        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         return ResponseEntity.notFound().build();
 
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+       return cozinhaRepository.salvar(cozinha);
     }
 
 }
