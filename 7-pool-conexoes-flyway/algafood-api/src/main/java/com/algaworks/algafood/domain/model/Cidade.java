bb1e79447872d935dec2dd3e6a1ecd3ac77acc09
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-01-29 16:05:21 -0300

    Configurando o pool de conexões do Hikari

---
+++ 7-pool-conexoes-flyway/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cidade.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cidade {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Estado estado;
+
+}
