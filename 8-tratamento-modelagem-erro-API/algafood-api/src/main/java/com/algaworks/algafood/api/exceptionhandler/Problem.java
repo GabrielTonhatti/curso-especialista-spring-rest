c15d20ad08c04a67ab4dd7aaed31c98ee47a1d34
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-02-03 22:49:32 -0300

    Customizando exception handlers de ResponseEntityExceptionHandler

---
+++ 8-tratamento-modelagem-erro-API/algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafood.api.exceptionhandler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Problem {
+
+    private Integer status;
+    private String type;
+    private String title;
+    private String detail;
+
+}
