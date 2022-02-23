4434fa8ae7a20736b4038e30692ad4598080e5e8
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-02-22 22:02:27 -0300

    Criando constraints de validação customizadas com implementação de ConstraintValidator

--- 9-validacoes-com-bean-validation/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ 9-validacoes-com-bean-validation/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,7 +1,7 @@
 package com.algaworks.algafood.domain.model;
 
 import com.algaworks.algafood.core.validation.Groups;
-import com.algaworks.algafood.core.validation.TaxaFrete;
+import com.algaworks.algafood.core.validation.Multiplo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -36,7 +36,7 @@ public class Restaurante {
 
     @NotNull
     @PositiveOrZero
-    @TaxaFrete
+    @Multiplo(numero = 5)
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
