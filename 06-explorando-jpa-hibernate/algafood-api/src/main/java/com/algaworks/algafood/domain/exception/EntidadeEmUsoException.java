25e5fbd3269b84ff8c71996d6d6bc2136e06d612
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-02-28 19:27:47 -0300

    Resolvendo o Problema do N+1 com fetch join na JPQL

---
+++ 06-explorando-jpa-hibernate/algafood-api/src/main/java/com/algaworks/algafood/domain/exception/EntidadeEmUsoException.java
@@ -0,0 +1,9 @@
+package com.algaworks.algafood.domain.exception;
+
+public class EntidadeEmUsoException extends RuntimeException {
+
+    public EntidadeEmUsoException(String mensagem) {
+        super(mensagem);
+    }
+
+}
---
+++ 06-explorando-jpa-hibernate/algafood-api/src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,9 @@
+package com.algaworks.algafood.domain.exception;
+
+public class EntidadeNaoEncontradaException extends RuntimeException {
+
+    public EntidadeNaoEncontradaException(String mensagem) {
+        super(mensagem);
+    }
+
+}
