92db2045952f99a9d066be8acc6eaf1a3a3112d3
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-04-19 23:49:30 -0300

    Refatorando o código de regras para transição de status de pedidos

--- 12-modelagem-avancada-implementacao-API/algafood-api/src/main/java/com/algaworks/algafood/api/controller/FluxoPedidoController.java
+++ 12-modelagem-avancada-implementacao-API/algafood-api/src/main/java/com/algaworks/algafood/api/controller/FluxoPedidoController.java
@@ -1,24 +1,9 @@
 package com.algaworks.algafood.api.controller;
 
-import com.algaworks.algafood.api.assembler.PedidoInputDisassembler;
-import com.algaworks.algafood.api.assembler.PedidoModelAssembler;
-import com.algaworks.algafood.api.assembler.PedidoResumoModelAssembler;
-import com.algaworks.algafood.api.model.PedidoModel;
-import com.algaworks.algafood.api.model.PedidoResumoModel;
-import com.algaworks.algafood.api.model.input.PedidoInput;
-import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
-import com.algaworks.algafood.domain.exception.NegocioException;
-import com.algaworks.algafood.domain.model.Pedido;
-import com.algaworks.algafood.domain.model.Usuario;
-import com.algaworks.algafood.domain.repository.PedidoRepository;
-import com.algaworks.algafood.domain.service.EmissaoPedidoService;
 import com.algaworks.algafood.domain.service.FluxoPedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/pedidos/{pedidoId}")
--- 12-modelagem-avancada-implementacao-API/algafood-api/src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ 12-modelagem-avancada-implementacao-API/algafood-api/src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -18,46 +18,20 @@ public class FluxoPedidoService {
     @Transactional
     public void confirmar(Long pedidoId) {
         Pedido pedido = emissaoPedidoService.buscarOuFalhar(pedidoId);
-
-        if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
-            throw new NegocioException(
-                    String.format("Status do pedido %d não pode ser alterado de %s para %s.",
-                            pedido.getId(), pedido.getStatus().getDescricao(), StatusPedido.CONFIRMADO.getDescricao())
-            );
-        }
-
-        pedido.setStatus(StatusPedido.CONFIRMADO);
-        pedido.setDataConfirmacao(OffsetDateTime.now());
+        pedido.confirmar();
     }
 
     @Transactional
     public void entregar(Long pedidoId) {
         Pedido pedido = emissaoPedidoService.buscarOuFalhar(pedidoId);
-
-        if (!pedido.getStatus().equals(StatusPedido.CONFIRMADO)) {
-            throw new NegocioException(
-                    String.format("Status do pedido %d não pode ser alterado de %s para %s.",
-                            pedido.getId(), pedido.getStatus().getDescricao(), StatusPedido.ENTREGUE.getDescricao())
-            );
-        }
-
-        pedido.setStatus(StatusPedido.ENTREGUE);
-        pedido.setDataEntrega(OffsetDateTime.now());
+        pedido.entregar();
     }
 
     @Transactional
     public void cancelar(Long pedidoId) {
         Pedido pedido = emissaoPedidoService.buscarOuFalhar(pedidoId);
+        pedido.cancelar();
 
-        if (!pedido.getStatus().equals(StatusPedido.CRIADO)) {
-            throw new NegocioException(
-                    String.format("Status do pedido %d não pode ser alterado de %s para %s.",
-                            pedido.getId(), pedido.getStatus().getDescricao(), StatusPedido.CANCELADO.getDescricao())
-            );
-        }
-
-        pedido.setStatus(StatusPedido.CANCELADO);
-        pedido.setDataCancelamento(OffsetDateTime.now());
     }
 
 }
