72b71ee4515bdc0769a5f1cc96679971d6b8e2b6
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-04-25 20:34:09 -0300

    Gerando a definição OpenAPI em JSON com SpringFox

--- 18-documentacai-api-openAPI-swagger-ui-spring-fox/algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ 18-documentacai-api-openAPI-swagger-ui-spring-fox/algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -3,7 +3,6 @@ package com.algaworks.algafood.core.openapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
