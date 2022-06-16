93ed06604140ae7055931d151d836aea5916eb1b
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-06-16 18:26:18 -0300

    Configurando o Resource Server para JWT assinado com chave simétrica

--- 23-oauth2-avancao-com-jwt-e-controle-acesso/algafood-api/src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ 23-oauth2-avancao-com-jwt-e-controle-acesso/algafood-api/src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,9 +1,14 @@
 package com.algaworks.algafood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +24,18 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 .cors()
                 .and()
                 .oauth2ResourceServer()
-                .opaqueToken();
+                .jwt();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        var secretKey = new SecretKeySpec(
+                "ADASDHF*-DRADE3ewrwijk432EWIhuLJKSLUJWR43538FH~ADADA".getBytes(), // chave secreta deve ter 256 bits no minimo
+                "HmacSHA256");
+
+        return NimbusJwtDecoder
+                .withSecretKey(secretKey)
+                .build();
     }
 
 }
--- 23-oauth2-avancao-com-jwt-e-controle-acesso/algafood-auth/src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ 23-oauth2-avancao-com-jwt-e-controle-acesso/algafood-auth/src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,6 +15,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 import java.util.Arrays;
@@ -30,9 +32,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Autowired
     private UserDetailsService userDetailsService;
-
-    @Autowired
-    private RedisConnectionFactory redisConnectionFactory;
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -79,8 +78,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService)
                 .reuseRefreshTokens(false)
-                .tokenStore(redisTokenStore())
+                .accessTokenConverter(jwtAccessTokenConverter())
                 .tokenGranter(tokenGranter(endpoints));
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey("ADASDHF*-DRADE3ewrwijk432EWIhuLJKSLUJWR43538FH~ADADA");
+        return jwtAccessTokenConverter;
     }
 
     private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
@@ -93,10 +99,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         );
 
         return new CompositeTokenGranter(granters);
-    }
-
-    private TokenStore redisTokenStore() {
-        return new RedisTokenStore(redisConnectionFactory);
     }
 
 }
