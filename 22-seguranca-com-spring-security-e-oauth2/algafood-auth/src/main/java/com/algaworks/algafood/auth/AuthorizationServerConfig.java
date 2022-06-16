e119583ce0ef10f142cc0aa68cb36e82fee904f8
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-06-15 22:05:16 -0300

    Configurando o Authorization Code Grant Type

--- 22-seguranca-com-spring-security-e-oauth2/algafood-auth/src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ 22-seguranca-com-spring-security-e-oauth2/algafood-auth/src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -35,6 +35,17 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .accessTokenValiditySeconds(60 * 60 * 60) // 6 horas
                 .refreshTokenValiditySeconds(60 * 60 * 24 * 60) // 60 dias
                 .and()
+                .withClient("foodanalytics")
+                .secret(passwordEncoder.encode("food123"))
+                .authorizedGrantTypes("authorization_code")
+                .scopes("write", "read")
+                .redirectUris("http://aplicacao-ciente")
+                .and()
+                .withClient("faturamento")
+                .secret(passwordEncoder.encode("faturamento123"))
+                .authorizedGrantTypes("client_credentials")
+                .scopes("write", "read")
+                .and()
                 .withClient("checktoken")
                 .secret(passwordEncoder.encode("check123"));
     }
