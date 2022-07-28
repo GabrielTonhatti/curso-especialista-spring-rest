c264fedf1e928fbf225bd2385e88b58f616b183a
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-07-28 19:24:36 -0300

    Configurando múltiplas documentações em um só projeto

--- 26-documentacao-api-com-springDoc/algafood-api/src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
+++ 26-documentacao-api-com-springDoc/algafood-api/src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
@@ -4,19 +4,44 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringDocConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .info(info())
-                .externalDocs(externalDocs())
+//    @Bean
+//    public OpenAPI openAPI() {
+//        return new OpenAPI()
+//                .info(info())
+//                .externalDocs(externalDocs());
+//    }
 
-                ;
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi
+                .builder()
+                .group("AlgaFood API Admin")
+                .pathsToMatch("/v1/**")
+                .addOpenApiCustomiser(openApi -> openApi
+                        .info(info("Admin"))
+                        .externalDocs(externalDocs())
+                )
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi groupedOpenApiCliente() {
+        return GroupedOpenApi
+                .builder()
+                .group("AlgaFood API Cliente")
+                .pathsToMatch("/cliente/v1/**")
+                .addOpenApiCustomiser(openApi -> openApi
+                        .info(info("Cliente"))
+                        .externalDocs(externalDocs())
+                )
+                .build();
     }
 
     private static ExternalDocumentation externalDocs() {
@@ -25,9 +50,9 @@ public class SpringDocConfig {
                 .url("https://algaworks.com");
     }
 
-    private static Info info() {
+    private static Info info(String info) {
         return new Info()
-                .title("AlgaFood API")
+                .title("AlgaFood API " + info)
                 .description("REST API do AlgaFood")
                 .license(new License()
                         .name("Apache 2.0")
