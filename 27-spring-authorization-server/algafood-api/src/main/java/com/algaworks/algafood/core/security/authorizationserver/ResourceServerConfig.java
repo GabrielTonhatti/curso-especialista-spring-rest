05ae9f02e9025bc5a2928617563a5bc5de9de6d9
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-09-12 13:38:19 -0300

    Configurando o Resource Server com token opaco

---
+++ 27-spring-authorization-server/algafood-api/src/main/java/com/algaworks/algafood/core/security/authorizationserver/ResourceServerConfig.java
@@ -0,0 +1,33 @@
+package com.algaworks.algafood.core.security.authorizationserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceServerConfig {
+
+    @Bean
+    public SecurityFilterChain resoucrServerFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/oauth2/**")
+                .authenticated()
+                .and()
+                .csrf()
+                .disable()
+                .cors()
+                .and()
+                .oauth2ResourceServer()
+                .opaqueToken();
+
+        return http
+                .build();
+    }
+
+}
