fa11369ca79e4cd201cdc9d4468afe4e57b5d8d7
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-09-12 13:15:35 -0300

    Configuração inicial do Authorization Server com Access Token opaco

---
+++ 27-spring-authorization-server/algafood-api/src/main/java/com/algaworks/algafood/core/security/authorizationserver/AlgaFoodSecutiryProperties.java
@@ -0,0 +1,21 @@
+package com.algaworks.algafood.core.security.authorizationserver;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@Validated
+@Component
+@ConfigurationProperties("algafood.auth")
+public class AlgaFoodSecutiryProperties {
+
+    @NotBlank
+    private String providerUrl;
+
+}
