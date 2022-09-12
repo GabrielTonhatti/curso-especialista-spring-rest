a3628d571cd4a8f20103650b60b5d55c925dc2ab
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-09-12 13:51:48 -0300

    Armazenando autorizações no banco de dados

--- 27-spring-authorization-server/algafood-api/src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ 27-spring-authorization-server/algafood-api/src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -4,12 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.InMemoryOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -56,8 +60,13 @@ public class AuthorizationServerConfig {
                 )
                 .build();
 
-
         return new InMemoryRegisteredClientRepository(List.of(algafoodbackend));
+    }
+
+    @Bean
+    public OAuth2AuthorizationService authorizationService(JdbcOperations jdbcOperations,
+                                                           RegisteredClientRepository registeredClientRepository) {
+        return new JdbcOAuth2AuthorizationService(jdbcOperations, registeredClientRepository);
     }
 
 }
