dd5e7bd412e1c7346fd3963fa315c1037d669755
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-09-13 13:48:24 -0300

    Testando o fluxo Authorization Code + PKCE + S256 e corrigindo problemas

--- 27-spring-authorization-server/algafood-api/src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ 27-spring-authorization-server/algafood-api/src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -11,27 +11,25 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.OAuth2TokenFormat;
-import org.springframework.security.oauth2.server.authorization.InMemoryOAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
 import org.springframework.security.oauth2.server.authorization.config.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.security.KeyStore;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
 import java.time.Duration;
 import java.util.List;
 
@@ -42,7 +40,9 @@ public class AuthorizationServerConfig {
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityFilterChain authFilterChain(HttpSecurity http) throws Exception {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-        return http.build();
+        return http
+                .formLogin(Customizer.withDefaults())
+                .build();
     }
 
     @Bean
@@ -71,7 +71,56 @@ public class AuthorizationServerConfig {
                 )
                 .build();
 
-        return new InMemoryRegisteredClientRepository(List.of(algafoodbackend));
+        RegisteredClient algafoodweb = RegisteredClient
+                .withId("2")
+                .clientId("algafood-web")
+                .clientSecret(passwordEncoder.encode("web123"))
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .scope("READ")
+                .scope("WRITE")
+                .tokenSettings(
+                        TokenSettings
+                                .builder()
+                                .accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
+                                .accessTokenTimeToLive(Duration.ofMinutes(15))
+                                .build()
+                )
+                .redirectUri("http://http://127.0.0.1:8080/authorized")
+                .redirectUri("http://http://127.0.0.1:8080/swagger-ui/oauth2-redirect.html")
+                .clientSettings(
+                        ClientSettings
+                                .builder()
+                                .requireAuthorizationConsent(true)
+                                .build()
+                )
+                .build();
+
+        RegisteredClient foodanalytics = RegisteredClient
+                .withId("3")
+                .clientId("foodanalytics")
+                .clientSecret(passwordEncoder.encode("web123"))
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .scope("READ")
+                .scope("WRITE")
+                .tokenSettings(
+                        TokenSettings
+                                .builder()
+                                .accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
+                                .accessTokenTimeToLive(Duration.ofMinutes(30))
+                                .build()
+                )
+                .redirectUri("http://www.foodanalytics.local:8082")
+                .clientSettings(
+                        ClientSettings
+                                .builder()
+                                .requireAuthorizationConsent(false)
+                                .build()
+                )
+                .build();
+
+        return new InMemoryRegisteredClientRepository(List.of(algafoodbackend, algafoodweb, foodanalytics));
     }
 
     @Bean
--- 27-spring-authorization-server/algafood-api/src/main/java/com/algaworks/algafood/core/security/authorizationserver/ResourceServerConfig.java
+++ 27-spring-authorization-server/algafood-api/src/main/java/com/algaworks/algafood/core/security/authorizationserver/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.core.security.authorizationserver;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,6 +28,7 @@ public class ResourceServerConfig {
                 .jwt();
 
         return http
+                .formLogin(Customizer.withDefaults())
                 .build();
     }
 
