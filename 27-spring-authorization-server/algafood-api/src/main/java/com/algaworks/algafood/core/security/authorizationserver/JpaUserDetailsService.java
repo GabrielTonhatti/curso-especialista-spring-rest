6e792e5ba1b4062f9a6c9a2e0e9ac4bd0b5bbaaf
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-09-14 08:29:28 -0300

    Testando o fluxo Authorization Code + PKCE + S256 e corrigindo problemas

--- 27-spring-authorization-server/algafood-api/src/main/java/com/algaworks/algafood/core/security/authorizationserver/JpaUserDetailsService.java
+++ 27-spring-authorization-server/algafood-api/src/main/java/com/algaworks/algafood/core/security/authorizationserver/JpaUserDetailsService.java
@@ -5,6 +5,7 @@ import com.algaworks.algafood.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -27,7 +28,7 @@ public class JpaUserDetailsService implements UserDetailsService {
                 .findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com e-mail informado!"));
 
-        return new AuthUser(usuario, getAuthorities(usuario));
+        return new User(usuario.getEmail(), usuario.getSenha(), getAuthorities(usuario));
     }
 
     private Collection<GrantedAuthority> getAuthorities(Usuario usuario) {
